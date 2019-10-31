d21f8ad36a0e2cb4ee66b055b60bf60e4b87b136
Ayako Wiltshire <awittyl44@gmail.com>
2019-10-31 21:26:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,13 +61,15 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+
 
 	programmer_hash = 
  		{
+ 		    :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        },
+ 		  
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -81,7 +83,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:yukihiro_matsumoto]
     
 end
 
